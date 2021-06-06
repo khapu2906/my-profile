ba1570a75b578b5437751f583acd825a54eda0a3
KHAPU <daikhanh9260@gmail.com>
2021-06-07 03:21:03 +0700

    ddayaar toafnb ộ

---
+++ forms/contact.php
@@ -0,0 +1,33 @@
+<?php
+  /**
+  * Requires the "PHP Email Form" library
+  * The "PHP Email Form" library is available only in the pro version of the template
+  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
+  * For more info and help: https://bootstrapmade.com/php-email-form/
+  */
+
+  // Replace contact@example.com with your real receiving email address
+  $receiving_email_address = 'daikhanh9260@gmail.com';
+
+  $contact = new stdClass;
+  $contact->ajax = true;
+  
+  $contact->to = $receiving_email_address;
+  $contact->from_name = $_POST['name'];
+  $contact->from_email = $_POST['email'];
+  $contact->subject = $_POST['subject'];
+  $contact->message = $_POST['message'];
+  $contact->header = "From:".$_POST['email']."\r\n";
+
+  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
+  /*
+  $contact->smtp = array(
+    'host' => 'example.com',
+    'username' => 'example',
+    'password' => 'pass',
+    'port' => '587'
+  );
+  */
+
+  $success = mail ($contact->to,$contact->subject,$contact->message,$contact->header);
+?>
